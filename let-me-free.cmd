ffd372a3675076c916d7846ed1af2a0097df6f56
jonarango <78152427+jonarango@users.noreply.github.com>
2024-10-30 09:35:49 +0100

    Create let-me-free.cmd with basic code

---
+++ let-me-free.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo [!] - Asking For Administrator!
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+echo [!] - Succes! Continuing...
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies"
+echo [!] - Deleted Domain Limitations: HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+echo [!] - Deleted Mouse Domain Limitations: HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\DataCollection
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+echo [!] - Deleted Update Limitations: HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate
+echo [!] - Aplying Changes!
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+echo [!] - Done!
+pause > null && del null
